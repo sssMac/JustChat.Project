3be854e4f68d29f342e49fe97e4ef251c10d6226
Ilya <ilya-lyapin@mail.ru>
2023-05-25 09:12:13 +0300

    dsad

--- Netflex.Statistic/Kafka.Consumer/Models/StatisticProcessingRequest.cs
+++ Netflex.Statistic/Kafka.Consumer/Models/StatisticProcessingRequest.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ThirdParty.Json.LitJson;
-
+﻿
+using Newtonsoft.Json;
 namespace Kafka.Consumer.Models
 {
     public class StatisticProcessingRequest
     {
         [JsonProperty("id")]
         public Guid Id { get; set; }
+
         [JsonProperty("name")]
         public string Name { get; set; }
     }
